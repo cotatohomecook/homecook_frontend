866bbda9e55ac9a1202a4e272a8a94f45b3020f9
kkaerrung <yujeong6231@naver.com>
2023-05-12 17:35:55 +0900

    test

--- App.js
+++ App.js
@@ -13,8 +13,8 @@ export default function App() {
      
       <NavigationContainer>
         <Stack.Navigator>
-          <Stack.Screen name="CostomerStartScreen" component={CostomerStartScreen} />
-          <Stack.Screen name="CostomerMap" component ={CostomerMap}/>
+          <Stack.Screen name="CostomerStartScreen" component={CostomerStartScreen} options={{headerShown: false}} />
+          <Stack.Screen name="CostomerMap" component ={CostomerMap} options={{headerShown: false}}/>
         </Stack.Navigator>
       </NavigationContainer>
     
