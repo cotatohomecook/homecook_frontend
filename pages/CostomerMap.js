8f31622bc99c051e365ad65dd26b5b2dee399d65
kkaerrung <yujeong6231@naver.com>
2023-05-16 00:04:14 +0900

    🚩 지도 화면 생성

--- pages/CostomerMap.js
+++ pages/CostomerMap.js
@@ -1,9 +1,54 @@
-import { Text} from "react-native";
-function CostomerMap() {
-    return(
-   <Text>Hi</Text>
-   )
+import { useState } from 'react';
+import { StyleSheet } from 'react-native';
+import MapView, { Marker } from 'react-native-maps';
+
+function Map({navigation}) {
+  const [selectedLocation, setSelectedLocation] = useState();
+
+  const region = {
+    latitude: 37.78,
+    longitude: -122.43,
+    latitudeDelta: 0.0922,
+    longitudeDelta: 0.0421,
+  };
+
+  function selectLocationHandler(event) {
+    const lat = event.nativeEvent.coordinate.latitude;
+    const lng = event.nativeEvent.coordinate.longitude;
+
+    setSelectedLocation({ lat: lat, lng: lng });
+  }
+  function savePickedLocationHandler(){
+    if(!selectedLocation){
+        Alert.alert('지도를 터치해서, 위치를 먼저 선택해야 합니다')
+    }
+    return;
+  }
+  navigation.goBack();
+
+  return (
+    <MapView
+      style={styles.map}
+      initialRegion={region}
+      onPress={selectLocationHandler}
+    >
+      {selectedLocation && (
+        <Marker
+          title="Picked Location"
+          coordinate={{
+            latitude: selectedLocation.lat,
+            longitude: selectedLocation.lng,
+          }}
+        />
+      )}
+    </MapView>
+  );
 }
-export default CostomerMap;
 
+export default Map;
 
+const styles = StyleSheet.create({
+  map: {
+    flex: 1,
+  },
+});
