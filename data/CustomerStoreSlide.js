f7144122f118597c04d97c3513d97791cfc4f136
kkaerrung <yujeong6231@naver.com>
2023-05-27 14:51:49 +0900

    🎨 슬라이드 구현 시 carousel 사용하도록 변경

--- data/CustomerStoreSlide.js
+++ data/CustomerStoreSlide.js
@@ -1,14 +1,21 @@
+import React, { useState, useEffect, useRef } from "react";
 import {
   Text,
   View,
   StyleSheet,
-  ScrollView,
   Dimensions,
   Image,
   Pressable,
 } from "react-native";
-import React, { useState, useEffect, useRef } from "react";
+import Carousel from "react-native-snap-carousel";
 import axios from "axios";
+
+const latitude = 37.602643;
+const longitude = 126.924805;
+const itemWidth = 160;
+const sliderWidth = Dimensions.get("window").width;
+
+const { width } = Dimensions.get("window");
 
 function CustomerStoreSlide() {
   const [news, setNews] = useState([]);
@@ -16,13 +23,7 @@ function CustomerStoreSlide() {
   const [error, setError] = useState(null);
   const [focusedIndex, setFocusedIndex] = useState(0);
 
-  const scrollViewRef = useRef(null);
-  const { width: screenWidth } = Dimensions.get("window");
-  const itemWidth = 160;
-  const itemMargin = 10;
-  const visibleItemCount = Math.floor(
-    screenWidth / (itemWidth + itemMargin * 2)
-  );
+  const carouselRef = useRef(null);
 
   const fetchNews = async () => {
     try {
@@ -31,7 +32,7 @@ function CustomerStoreSlide() {
       setNews([]);
 
       const response = await axios.get(
-        "http://3.38.33.21:8080/api/shops/random?latitude=37.602643&longitude=126.924805"
+        `http://3.38.33.21:8080/api/shops/random?latitude=${latitude}&longitude=${longitude}`
       );
 
       setNews(response.data.data);
@@ -46,88 +47,43 @@ function CustomerStoreSlide() {
     fetchNews();
   }, []);
 
-  const handleScroll = (event) => {
-    const contentOffsetX = event.nativeEvent.contentOffset.x;
-    const index = Math.floor(contentOffsetX / (itemWidth + itemMargin * 2));
-    const fractionalOffset =
-      contentOffsetX / (itemWidth + itemMargin * 2) - index;
-
-    if (fractionalOffset >= 0.8 && index < news.length - 1) {
-      setFocusedIndex(index + 1);
-    } else {
-      setFocusedIndex(index);
-    }
-  };
-
-  useEffect(() => {
-    if (
-      scrollViewRef.current &&
-      focusedIndex >= 0 &&
-      focusedIndex < news.length
-    ) {
-      const offset = focusedIndex * (itemWidth + itemMargin * 2);
-      const maxOffset = Math.max(
-        0,
-        news.length * (itemWidth + itemMargin * 2) - screenWidth
-      );
-      const finalOffset = Math.min(maxOffset, offset);
-      scrollViewRef.current.scrollTo({ x: finalOffset, animated: true });
-    }
-  }, [focusedIndex, news]);
-
-  if (loading)
-    return (
-      <View style={{ margin: 50, width: 300, height: 200 }}>
-        <Text>로딩 중..</Text>
-      </View>
-    );
-  if (error)
-    return (
-      <View style={{ margin: 50, width: 300, height: 200 }}>
-        <Text>에러가 발생했습니다.</Text>
-      </View>
-    );
-  if (!news.length) return null;
+  const renderItem = ({ item, index }) => (
+    <View style={styles.square}>
+      <Pressable>
+        <Image
+          style={styles.image}
+          source={{ uri: item.imageUrl, width: 141, height: 90, top: -10 }}
+        />
+        <Text style={styles.label1}>{item.shopName}</Text>
+        <Text style={styles.label2}>{item.bestMenuName}</Text>
+      </Pressable>
+    </View>
+  );
 
   return (
-    <ScrollView
-      horizontal
-      ref={scrollViewRef}
-      contentContainerStyle={styles.scrollViewContent}
-      showsHorizontalScrollIndicator={false}
-      onScroll={handleScroll}
-    >
-      {news.map((item, index) => (
-        <View
-          key={index}
-          style={[
-            styles.square,
-            { width: itemWidth },
-            index === focusedIndex
-              ? styles.focusedSquare
-              : styles.unfocusedSquare,
-          ]}
-        >
-          <Pressable>
-            <Image
-              style={styles.image}
-              source={{ uri: item.imageUrl, width: 141, height: 90, top: -10 }}
-            />
-            <Text style={styles.label1}>{item.shopName}</Text>
-            <Text style={styles.label2}>{item.bestMenuName}</Text>
-          </Pressable>
-        </View>
-      ))}
-    </ScrollView>
+    <View style={styles.container}>
+      <Carousel
+        ref={carouselRef}
+        data={news}
+        renderItem={renderItem}
+        itemWidth={itemWidth}
+        inactiveSlideOpacity={0.5}
+        sliderWidth={width}
+        firstItem={0}
+      />
+    </View>
   );
 }
 
 export default CustomerStoreSlide;
 
 const styles = StyleSheet.create({
-  scrollViewContent: {
-    flexDirection: "row",
+  container: {
+    flex: 1,
     alignItems: "center",
+    justifyContent: "center",
+    top: 130,
+    left: 50,
   },
   square: {
     width: 160,
@@ -138,19 +94,13 @@ const styles = StyleSheet.create({
     alignItems: "center",
     borderRadius: 30,
   },
-  focusedSquare: {
-    opacity: 0.9,
-  },
-  unfocusedSquare: {
-    opacity: 0.1,
-  },
   label1: {
     position: "absolute",
     width: 79,
     height: 19,
     left: 30,
     top: 80,
-    fontWeight: 700,
+    fontWeight: "700",
     fontSize: 13,
     color: "#FFFFFF",
     textAlign: "center",
@@ -161,14 +111,12 @@ const styles = StyleSheet.create({
     height: 14,
     left: 50,
     top: 99,
-    fontWeight: 500,
+    fontWeight: "500",
     fontSize: 10,
     textAlign: "center",
     color: "#FFFFFF",
   },
   image: {
     top: -20,
-    //////
-    /////
   },
 });
