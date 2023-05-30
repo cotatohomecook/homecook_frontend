3cd8400746de758605f0e171de6f64905ac416b8
yujeongFE <yujeong6231@naver.com>
2023-05-30 17:38:02 +0900

    🚩 검색 모달창 생성

--- App.js
+++ App.js
@@ -10,132 +10,162 @@ import ListScreen from "./ButtomTab/ListScreen";
 import BookmarkScreen from "./ButtomTab/BookmarkScreen";
 import MypageScreen from "./ButtomTab/MypageScreen";
 import { View } from "react-native-animatable";
+import React, { useState } from "react";
+import { Modal, Text, TouchableOpacity } from "react-native";
 
 const Stack = createNativeStackNavigator();
 const Tab = createBottomTabNavigator();
 
 function BottomTabNavigator() {
+  const [searchModalVisible, setSearchModalVisible] = useState(false);
+
+  const openSearchModal = () => {
+    setSearchModalVisible(true);
+  };
+
+  const closeSearchModal = () => {
+    setSearchModalVisible(false);
+  };
+
   return (
-    <Tab.Navigator
-      screenOptions={({ route }) => ({
-        tabBarStyle: { backgroundColor: "#ffb15f", height: 51 }, // 탭의 배경색과 높이
-        tabBarIconStyle: { marginBottom: 1, marginTop: 10 },
-        tabBarLabelStyle: { display: "none" },
-      })}
-    >
-      <Tab.Screen
-        name="List"
-        component={ListScreen}
-        options={{
-          headerShown: false,
-          tabBarIcon: ({ focused }) => (
-            <Image
-              source={{
-                uri: "https://velog.velcdn.com/images/thgus05061/post/9945ec5c-1f25-4992-93d6-f20dfe86c18e/image.png",
-              }}
-              style={{
-                width: 18,
-                height: 20.5,
-              }}
-            />
-          ),
-        }}
-      />
-
-      <Tab.Screen
-        name="Bookmark"
-        component={BookmarkScreen}
-        options={{
-          headerShown: false,
-          tabBarIcon: ({ focused }) => (
-            <Image
-              source={{
-                uri: "https://velog.velcdn.com/images/thgus05061/post/2018910e-32b1-4d7f-aeb9-7d7e9e7c712f/image.png",
-              }}
-              style={{
-                width: 27,
-                height: 27,
-              }}
-            />
-          ),
-        }}
-      />
-
-      <Tab.Screen
-        name="CustomerStartScreen"
-        component={CustomerStartScreen}
-        options={{
-          headerShown: false,
-          tabBarIcon: ({ focused }) => (
-            <View
-              style={{
-                top: -20,
-                width: 71,
-                height: 71,
-                borderRadius: 100,
-                alignItems: "center",
-                justifyContent: "center",
-                backgroundColor: "#ffb15f",
-                shadowColor: "#000",
-                shadowOffset: {
-                  width: 0,
-                  height: 2,
-                },
-                shadowOpacity: 0.3,
-                shadowRadius: 4,
-                elevation: 4,
-              }}
-            >
+    <View style={{ flex: 1 }}>
+      <Tab.Navigator
+        initialRouteName="CustomerStartScreen"
+        screenOptions={({ route }) => ({
+          tabBarStyle: { backgroundColor: "#ffb15f", height: 51 },
+          tabBarIconStyle: { marginBottom: 1, marginTop: 10 },
+          tabBarLabelStyle: { display: "none" },
+        })}
+      >
+        <Tab.Screen
+          name="List"
+          component={ListScreen}
+          options={{
+            headerShown: false,
+            tabBarIcon: ({ focused }) => (
               <Image
                 source={{
-                  uri: "https://velog.velcdn.com/images/thgus05061/post/29e392b5-ae80-4edc-9492-56b5b868971d/image.png",
+                  uri: "https://velog.velcdn.com/images/thgus05061/post/9945ec5c-1f25-4992-93d6-f20dfe86c18e/image.png",
                 }}
                 style={{
-                  width: 27.8,
-                  height: 27.4,
+                  width: 18,
+                  height: 20.5,
                 }}
               />
-            </View>
-          ),
-        }}
-      />
-      <Tab.Screen
-        name="Search"
-        component={SearchScreen}
-        options={{
-          headerShown: false,
-          tabBarIcon: ({ focused }) => (
-            <Image
-              source={{
-                uri: "https://velog.velcdn.com/images/thgus05061/post/24bc745f-f288-4cec-b0f9-e0e46a60f889/image.png",
-              }}
-              style={{
-                width: 18,
-                height: 18,
-              }}
-            />
-          ),
-        }}
-      />
-      <Tab.Screen
-        name="Mypage"
-        component={MypageScreen}
-        options={{
-          headerShown: false,
-          tabBarIcon: ({ focused }) => (
-            <Image
-              source={{
-                uri: "https://velog.velcdn.com/images/thgus05061/post/d4caa6f3-826b-4fb5-910f-6d34726fd7c4/image.png",
-              }}
-              style={{
-                width: 19,
-                height: 19,
-              }}
-            />
-          ),
-        }}
-      />
-    </Tab.Navigator>
+            ),
+          }}
+        />
+
+        <Tab.Screen
+          name="Bookmark"
+          component={BookmarkScreen}
+          options={{
+            headerShown: false,
+            tabBarIcon: ({ focused }) => (
+              <Image
+                source={{
+                  uri: "https://velog.velcdn.com/images/thgus05061/post/2018910e-32b1-4d7f-aeb9-7d7e9e7c712f/image.png",
+                }}
+                style={{
+                  width: 27,
+                  height: 27,
+                }}
+              />
+            ),
+          }}
+        />
+
+        <Tab.Screen
+          name="CustomerStartScreen"
+          component={CustomerStartScreen}
+          options={{
+            headerShown: false,
+            tabBarIcon: ({ focused }) => (
+              <View
+                style={{
+                  top: -20,
+                  width: 71,
+                  height: 71,
+                  borderRadius: 100,
+                  alignItems: "center",
+                  justifyContent: "center",
+                  backgroundColor: "#ffb15f",
+                  shadowColor: "#000",
+                  shadowOffset: {
+                    width: 0,
+                    height: 2,
+                  },
+                  shadowOpacity: 0.3,
+                  shadowRadius: 4,
+                  elevation: 4,
+                }}
+              >
+                <Image
+                  source={{
+                    uri: "https://velog.velcdn.com/images/thgus05061/post/29e392b5-ae80-4edc-9492-56b5b868971d/image.png",
+                  }}
+                  style={{
+                    width: 27.8,
+                    height: 27.4,
+                  }}
+                />
+              </View>
+            ),
+          }}
+        />
+        <Tab.Screen
+          name="SearchModal"
+          component={SearchScreen}
+          listeners={() => ({
+            tabPress: (e) => {
+              e.preventDefault();
+              openSearchModal();
+            },
+          })}
+          options={{
+            headerShown: false,
+            tabBarIcon: () => (
+              <Image
+                source={{
+                  uri: "https://velog.velcdn.com/images/thgus05061/post/24bc745f-f288-4cec-b0f9-e0e46a60f889/image.png",
+                }}
+                style={{
+                  width: 18,
+                  height: 18,
+                }}
+              />
+            ),
+          }}
+        />
+
+        <Tab.Screen
+          name="Mypage"
+          component={MypageScreen}
+          options={{
+            headerShown: false,
+            tabBarIcon: ({ focused }) => (
+              <Image
+                source={{
+                  uri: "https://velog.velcdn.com/images/thgus05061/post/d4caa6f3-826b-4fb5-910f-6d34726fd7c4/image.png",
+                }}
+                style={{
+                  width: 19,
+                  height: 19,
+                }}
+              />
+            ),
+          }}
+        />
+      </Tab.Navigator>
+
+      <Modal visible={searchModalVisible} animationType="slide" transparent>
+        <View style={styles.modalContainer}>
+          <View style={styles.modalContent}>
+            <SearchScreen closeModal={closeSearchModal} />
+          </View>
+        </View>
+      </Modal>
+    </View>
   );
 }
 
@@ -146,7 +176,7 @@ export default function App() {
       <NavigationContainer>
         <Stack.Navigator>
           <Stack.Screen
-            name="BottomTabNavigator"
+            name="CustomerStartScreen"
             component={BottomTabNavigator}
             options={{ headerShown: false }}
           />
@@ -166,6 +196,25 @@ export default function App() {
 }
 
 const styles = StyleSheet.create({
-  container: {},
-  // ...
+  modalContainer: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: "rgba(0, 0, 0, 0.5)",
+  },
+  modalContent: {
+    width: 371,
+    height: 724,
+    borderRadius: 21,
+    backgroundColor: "#ffffff",
+  },
+  modalText: {
+    fontSize: 18,
+    fontWeight: "bold",
+    marginBottom: 10,
+  },
+  closeButton: {
+    fontSize: 16,
+    color: "blue",
+  },
 });
--- ButtomTab/SearchScreen.js
+++ ButtomTab/SearchScreen.js
@@ -1,10 +1,47 @@
-import React from 'react'
-import { View,Text, StyleSheet } from 'react-native';
+import React from "react";
+import { StyleSheet, Text, TouchableOpacity, TextInput } from "react-native";
 
-function SearchScreen({navigation}) {
+const SearchScreen = ({ closeModal }) => {
   return (
-    <Text>Search</Text>
-  )
-}
+    <>
+      <TextInput
+        placeholder="검색어를 입력하세요"
+        style={styles.searchWord}
+        containerStyle={{ backgroundColor: "white" }}
+        inputContainerStyle={{ backgroundColor: "#e0e0e0" }}
+        cancelButtonProps={{ buttonTextStyle: { color: "gray" } }}
+      />
+      <TouchableOpacity onPress={closeModal}>
+        <Text style={styles.closeButton}>Close</Text>
+      </TouchableOpacity>
+    </>
+  );
+};
 
-export default SearchScreen
+export default SearchScreen;
+
+const styles = StyleSheet.create({
+  searchWord: {
+    width: 175,
+    height: 26,
+    fontSize: 18,
+    fontWeight: "500",
+    fontStyle: "normal",
+    letterSpacing: 0,
+    textAlign: "left",
+    color: "#afafaf",
+    left: 66,
+    top: 12,
+  },
+  searchHistory: {
+    width: 52,
+    height: 40,
+    fontSize: 14,
+    fontWeight: "500",
+    fontStyle: "normal",
+    letterSpacing: 0,
+    textAlign: "left",
+    color: "#7d7d7d",
+    paddingVertical: 5,
+  },
+});
