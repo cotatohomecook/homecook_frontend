92f216188d05d6aa9d2dfb8d623ed5822bc95265
yujeongFE <yujeong6231@naver.com>
2023-06-01 13:34:37 +0900

    💄 검색 모달창 내부 스타일 변경

--- ButtomTab/SearchScreen.js
+++ ButtomTab/SearchScreen.js
@@ -6,32 +6,24 @@ import {
   Text,
   TouchableOpacity,
   TextInput,
+  Image,
 } from "react-native";
 import { SearchBar } from "react-native-elements";
 import AsyncStorage from "@react-native-async-storage/async-storage";
 import DropDownPicker from "react-native-dropdown-picker";
 
-const SearchScreen = ({ closeModal }) => {
+const SearchScreen = ({ closeModal, navigation }) => {
   const [searchText, setSearchText] = useState("");
   const [searchResults, setSearchResults] = useState([]);
   const [searchHistory, setSearchHistory] = useState([]);
   const [searchType, setSearchType] = useState("");
 
   const [open, setOpen] = useState(false);
-  const [value, setValue] = useState(null);
+  const [value, setValue] = useState("상호명");
   const [items, setItems] = useState([
     { label: "상호명", value: "상호명" },
     { label: "메뉴명", value: "메뉴명" },
   ]);
-
-  // 검색 기록 초기화
-  const clearSearchHistory = async () => {
-    try {
-      setSearchHistory([]);
-    } catch (error) {
-      console.error("검색 기록 초기화 에러:", error);
-    }
-  };
 
   // 검색 기록 불러오기
   const loadSearchHistory = async () => {
@@ -59,6 +51,20 @@ const SearchScreen = ({ closeModal }) => {
     }
   };
 
+  //검색 기록 삭제하기
+  const deleteSearchHistory = async (index) => {
+    try {
+      const updatedHistory = searchHistory.filter((_, i) => i !== index);
+      await AsyncStorage.setItem(
+        "searchHistory",
+        JSON.stringify(updatedHistory)
+      );
+      setSearchHistory(updatedHistory);
+    } catch (error) {
+      console.error("검색 기록 삭제 에러:", error);
+    }
+  };
+
   const handleSearch = async () => {
     try {
       saveSearchHistory(searchText);
@@ -67,35 +73,45 @@ const SearchScreen = ({ closeModal }) => {
       );
       const data = await response.json();
       setSearchResults(data.data.content);
-
       setSearchText(""); // 검색 완료 후에 검색어 리셋
     } catch (error) {
       console.error("검색 에러:", error);
     }
   };
-
   const handleDropdownChange = (selectedValue) => {
     setSearchType(selectedValue);
   };
-
   useEffect(() => {
-    // 검색 기록 불러오기
     loadSearchHistory();
   }, []);
 
   return (
     <>
-      <DropDownPicker
-        open={open}
-        value={value}
-        items={items}
-        setOpen={setOpen}
-        setValue={setValue}
-        setItems={setItems}
-        placeholder="필터"
-        style={styles.dropdown}
-        onChangeValue={handleDropdownChange}
-      />
+      <View style={styles.filter}>
+        <DropDownPicker
+          open={open}
+          value={value}
+          items={items}
+          setOpen={setOpen}
+          setValue={setValue}
+          setItems={setItems}
+          onChangeValue={handleDropdownChange}
+          style={styles.dropdown}
+          dropDownContainerStyle={styles.dropdwoncontainer}
+          labelStyle={{ fontSize: 12, textAlign: "center", fontWeight: "bold" }}
+          arrowIconStyle={{
+            width: 20,
+            height: 20,
+            tintColor: "#F3AC61",
+          }}
+          showTickIcon={false}
+          listItemLabelStyle={{
+            fontSize: 12,
+            fontWeight: "bold",
+            textAlign: "center",
+          }}
+        />
+      </View>
       <TextInput
         placeholder="검색어를 입력하세요"
         style={styles.searchWord}
@@ -106,16 +122,45 @@ const SearchScreen = ({ closeModal }) => {
         inputContainerStyle={{ backgroundColor: "#e0e0e0" }}
         cancelButtonProps={{ buttonTextStyle: { color: "gray" } }}
       />
+      <TouchableOpacity onPress={closeModal}>
+        <Image
+          style={styles.backbutton}
+          source={{
+            uri: "https://velog.velcdn.com/images/kkaerrung/post/c7eb19ee-5f5d-4408-99e6-16f249b26398/image.png",
+            width: 20,
+            height: 20,
+          }}
+        />
+      </TouchableOpacity>
       <View style={styles.history}>
         <FlatList
           data={searchHistory.filter((item) => item.searchType === searchType)} // 검색 타입에 맞게 필터링
-          renderItem={({ item }) => <Text>{item.keyword}</Text>}
+          renderItem={({ item, index }) => (
+            <>
+              <View style={styles.searchcontainer}>
+                <Image
+                  source={{
+                    uri: "https://velog.velcdn.com/images/kkaerrung/post/0984f279-e46a-40b4-a890-00ea7b772eb9/image.png",
+                    width: 19,
+                    height: 16,
+                  }}
+                />
+                <Text style={styles.searchhistory}>{item.keyword}</Text>
+                <TouchableOpacity onPress={() => deleteSearchHistory(index)}>
+                  <Image
+                    style={styles.deleteButton}
+                    source={{
+                      uri: "https://velog.velcdn.com/images/kkaerrung/post/3db22830-f521-4e46-84fe-93e5469a8994/image.png",
+                      width: 19,
+                      height: 19,
+                    }}
+                  ></Image>
+                </TouchableOpacity>
+              </View>
+            </>
+          )}
           keyExtractor={(item, index) => index.toString()}
         />
-        <Text style={styles.clearbutton} onPress={clearSearchHistory}>
-          검색 기록 초기화
-        </Text>
-        <Text onPress={closeModal}>close</Text>
       </View>
     </>
   );
@@ -134,28 +179,61 @@ const styles = StyleSheet.create({
     textAlign: "left",
     color: "#afafaf",
     left: 66,
-    top: 100,
+    top: 30,
   },
-  searchHistory: {
+  searchhistory: {
     width: 52,
     height: 40,
     fontSize: 14,
+    left: 40,
+    top: 5,
     fontWeight: "500",
     fontStyle: "normal",
     letterSpacing: 0,
     textAlign: "left",
     color: "#7d7d7d",
     paddingVertical: 5,
-    top: 120,
+  },
+  searchcontainer: {
+    flexDirection: "row",
+    alignItems: "center",
+    left: 15,
+  },
+  dropdown: {
+    width: 100,
+    height: 29,
+    borderColor: "white",
+    borderRadius: 8,
+    elevation: 10,
+  },
+  dropdwoncontainer: {
+    width: 100,
+    height: 82,
+    borderColor: "white",
+    borderRadius: 8,
+    elevation: 15,
+    top: -10,
+  },
+  filter: {
+    position: "absolute",
+    left: 250,
+    top: 17,
+  },
+  backbutton: {
+    position: "absolute",
+    width: 20,
+    height: 20,
+    left: 16,
+    top: 6,
+  },
+  dropdownarrow: {
+    color: "F3AC61",
   },
   clearbutton: {
     top: 190,
   },
-  closebutton: {
-    top: 200,
+  deleteButton: {
+    left: 222,
   },
-  dropdown: {},
-  history: {
-    top: 100,
-  },
+  history: { top: 60 },
 });
