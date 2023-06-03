ff65d7ace0695eaa7e907a4caeaa0157e3a7b029
yujeongFE <yujeong6231@naver.com>
2023-06-03 14:43:27 +0900

    💄 검색 결과 표시 디자인 변경

--- components/Search/SearchResult.js
+++ components/Search/SearchResult.js
@@ -1,10 +1,20 @@
 import React, { useEffect, useState } from "react";
-import { View, Text, ActivityIndicator } from "react-native";
+import {
+  View,
+  Text,
+  ActivityIndicator,
+  TouchableOpacity,
+  StyleSheet,
+  Image,
+  FlatList,
+} from "react-native";
+import ContentBox from "../../common/ContentBox";
 
 const SearchResult = ({ route }) => {
   const { searchText, searchType } = route.params;
   const [searchResults, setSearchResults] = useState([]);
   const [loading, setLoading] = useState(false);
+  const [modalVisible, setModalVisible] = useState(false);
 
   useEffect(() => {
     const fetchSearchResults = async () => {
@@ -14,14 +24,11 @@ const SearchResult = ({ route }) => {
         const url =
           searchType === "상호명"
             ? `http://3.38.33.21:8080/api/shops/search?latitude=37.60264&longitude=126.924805&shopName=${searchText}&page=0&size=10&orderBy=distance`
-            : `http://3.38.33.21:8080/api/shops/map?latitude=37.602643&longitude=126.924805&menuName=${searchText}&page=0&size=10&orderBy=reviewCount`;
+            : `http://3.38.33.21:8080/api/shops/search?latitude=37.602643&longitude=126.924805&menuName=${searchText}&page=0&size=10&orderBy=reviewCount`;
 
         const response = await fetch(url);
         const data = await response.json();
-
-        setSearchResults(
-          searchType === "상호명" ? data.data.content : data.data
-        );
+        setSearchResults(data.data.content);
       } catch (error) {
         console.error("검색 에러:", error);
       } finally {
@@ -45,14 +52,33 @@ const SearchResult = ({ route }) => {
   }
 
   return (
-    <View>
-      <Text>검색 결과:</Text>
-      {searchResults.map((result) => (
-        <Text key={result.shopId}>{result.shopName}</Text>
-      ))}
-      <Text>검색어: {searchText}</Text>
-    </View>
+    <>
+      <FlatList
+        data={searchResults}
+        keyExtractor={(item) => item.shopId}
+        renderItem={({ item }) => (
+          <View style={styles.resultbox} key={item.shopId}>
+            <ContentBox
+              width={371}
+              height={116}
+              imgUrl={item.imageUrl}
+              title={item.shopName}
+              detail={item.bestMenuName}
+              rating={item.rating}
+            ></ContentBox>
+          </View>
+        )}
+      />
+    </>
   );
 };
 
 export default SearchResult;
+
+const styles = StyleSheet.create({
+  resultbox: {
+    elevation: 3,
+    marginBottom: 20,
+    alignItems: "center",
+  },
+});
