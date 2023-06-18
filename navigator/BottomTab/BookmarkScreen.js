60f4b6a916593b6c1859c30348262a9147cfabd9
yujeongFE <yujeong6231@naver.com>
2023-06-18 22:02:08 +0900

    ⚡️ 즐겨찾기 내역 확인 페이지 구현

--- navigator/BottomTab/BookmarkScreen.js
+++ navigator/BottomTab/BookmarkScreen.js
@@ -1,102 +1,183 @@
-import React from "react";
-import { View, TouchableOpacity, Text, StyleSheet, Image } from "react-native";
+import React, { useState, useEffect } from "react";
+import {
+  View,
+  TouchableOpacity,
+  Text,
+  StyleSheet,
+  Image,
+  ScrollView,
+} from "react-native";
+import { useNavigation } from "@react-navigation/native";
 import Header from "../../common/Header";
 import BackButton from "../../common/BackButton";
-
-const FavoriteCategoryButton = ({ title }) => {
-  return (
-    <TouchableOpacity style={styles.button}>
-      <View style={styles.buttonContent}>
-        <Image
-          source={{
-            uri: "https://velog.velcdn.com/images/kkaerrung/post/bd4f2268-be39-406a-90d8-6b8e94da9c7d/image.png",
-            width: 22,
-            height: 20,
-          }}
-        />
-        <Text style={styles.buttonText}>{title}</Text>
-        <Image
-          style={{ marginLeft: 170 }}
-          source={{
-            uri: "https://velog.velcdn.com/images/kkaerrung/post/a18cb522-3dd5-42b6-88d2-35e2d9424416/image.png",
-            width: 21,
-            height: 21,
-          }}
-        />
-      </View>
-    </TouchableOpacity>
-  );
-};
+import { useSelector, useDispatch } from "react-redux";
+import { fetchBookmarkData } from "../../store/redux/bookmark";
+import FavoriteCategoryButton from "../../common/FavoriteCategoryButton";
+import BookmarkListButton from "../../common/BookmarkListButton";
 
 const BookmarkScreen = () => {
+  const folderNames = useSelector((state) => state.bookmark.folderNames);
+  const folderData = useSelector((state) => state.bookmark.bookmarks);
+  const dispatch = useDispatch();
+  const [selectedFolder, setSelectedFolder] = useState(null);
+  const [showCategories, setShowCategories] = useState(true);
+
+  const navigation = useNavigation();
+
+  useEffect(() => {
+    dispatch(fetchBookmarkData());
+  }, []);
+
   const handleGoBack = () => {
-    navigation.goBack();
+    setSelectedFolder(null);
+    setShowCategories(true);
+  };
+
+  const handleCategoryPress = (category) => {
+    const selectedFolderData = folderData.filter(
+      (item) => item.folderName === category
+    );
+    setSelectedFolder(selectedFolderData.length > 0 ? category : null);
+    setShowCategories(false);
   };
 
   return (
-    <>
-      <Header height={114} title={"즐겨찾기"} />
-      <BackButton onPress={handleGoBack} top={-45} />
-      <Image
-        source={{
-          uri: "https://velog.velcdn.com/images/kkaerrung/post/d30e70af-cbc7-4ccf-a81e-03049d5d8ba9/image.png",
-          width: 78,
-          height: 78,
-        }}
-        style={{ marginTop: 17, marginLeft: 158 }}
-      ></Image>
-      <View style={styles.container}>
-        <View style={styles.buttonsContainer}>
-          <FavoriteCategoryButton title="한식" />
-          <FavoriteCategoryButton title="양식" />
-          <FavoriteCategoryButton title="베이커리" />
-          <FavoriteCategoryButton title="일식" />
-        </View>
-      </View>
-      <TouchableOpacity>
+    <ScrollView style={styles.container}>
+      <TouchableOpacity activeOpacity={0.8} style={styles.screen}>
+        <Header height={114} title={"즐겨찾기"} />
+        <BackButton onPress={handleGoBack} top={-45} />
         <Image
-          style={{ marginTop: 55, marginLeft: 166 }}
           source={{
-            uri: "https://velog.velcdn.com/images/kkaerrung/post/88b67bfb-585b-43e1-b49d-643690665248/image.png",
-            width: 61,
-            height: 61,
+            uri: "https://velog.velcdn.com/images/kkaerrung/post/d30e70af-cbc7-4ccf-a81e-03049d5d8ba9/image.png",
+            width: 78,
+            height: 78,
           }}
+          style={{ marginTop: 17, marginLeft: 32 }}
         />
+        <Text style={styles.textStyle}>
+          <Text style={styles.blueText}>'박세라'</Text>님{"\n"}
+          <Text style={styles.textSize}>즐겨찾기 내역이에요.</Text>
+        </Text>
+        {showCategories ? (
+          <View style={styles.buttonsContainer}>
+            {folderNames &&
+              folderNames.map((category) => (
+                <FavoriteCategoryButton
+                  key={category}
+                  title={category}
+                  onPress={() => handleCategoryPress(category)}
+                  style={styles.button}
+                >
+                  <Text style={styles.buttonText}>{category}</Text>
+                </FavoriteCategoryButton>
+              ))}
+          </View>
+        ) : (
+          <View style={styles.selectedFolderContainer}>
+            <View style={styles.selectedFolderBox}>
+              <Image
+                style={{ marginLeft: 90 }}
+                source={{
+                  uri: "https://velog.velcdn.com/images/kkaerrung/post/bd4f2268-be39-406a-90d8-6b8e94da9c7d/image.png",
+                  width: 22,
+                  height: 20,
+                }}
+              ></Image>
+              <Text style={styles.selectedFolderText}>{selectedFolder}</Text>
+            </View>
+            <TouchableOpacity onPress={handleGoBack}>
+              <Image
+                style={{ marginTop: 9 }}
+                source={{
+                  uri: "https://velog.velcdn.com/images/kkaerrung/post/8ff31b30-29cc-4a5f-a70d-2b1e340bab91/image.png",
+                  width: 19,
+                  height: 19,
+                }}
+              ></Image>
+            </TouchableOpacity>
+            {folderData.map(
+              (item) =>
+                item.folderName === selectedFolder && (
+                  <BookmarkListButton
+                    key={item.shopId}
+                    shopName={item.shopName}
+                    imageUrl={item.imageUrl}
+                  ></BookmarkListButton>
+                )
+            )}
+          </View>
+        )}
       </TouchableOpacity>
-    </>
+    </ScrollView>
   );
 };
 
 const styles = StyleSheet.create({
   container: {
-    top: 35,
-    alignItems: "center",
-  },
-  button: {
-    width: 322,
-    height: 42,
-    padding: 10,
-    marginVertical: 5,
+    flex: 1,
     backgroundColor: "#FFFFFF",
-    borderRadius: 9,
-    borderWidth: 0.8,
-    borderColor: "#C0C0C0",
   },
-  buttonContent: {
+  screen: {
+    flex: 1,
+    width: "100%",
+  },
+  textSize: {
+    fontSize: 24,
+  },
+  textStyle: {
+    width: 213,
+    height: 70,
+    left: 134,
+    top: -70,
+    fontWeight: "bold",
+    fontSize: 20,
+    textAlign: "left",
+  },
+  blueText: {
+    color: "#3D67FF",
+  },
+  selectedFolderBox: {
+    width: 258,
+    height: 52,
+    marginTop: -70,
+    backgroundColor: "#FFFFFF",
+    borderRadius: 27,
+    borderWidth: 0.5,
+    borderColor: "#000000",
     flexDirection: "row",
     alignItems: "center",
   },
-  activeButton: {
-    backgroundColor: "#2196F3",
+  selectedFolderContainer: {
+    alignItems: "center",
+    marginTop: 20,
   },
-  buttonText: {
-    fontSize: 16,
-    left: 18,
+  selectedFolderText: {
+    marginLeft: 10,
+    marginVertical: 10,
+    fontSize: 18,
     fontWeight: "bold",
-    color: "black",
-    marginLeft: 5,
-    marginRight: 5,
-    width: 66,
+  },
+  buttonsContainer: {
+    alignItems: "center",
+  },
+  borderBox: {
+    width: 327,
+    height: 135,
+    top: 322,
+    background: "#FFFFFF",
+    borderRadius: 10,
+    borderWidth: 0.5,
+  },
+  shopName: {
+    fontSize: 18,
+    fontWeight: "bold",
+    marginVertical: 10,
+  },
+  image: {
+    width: 200,
+    height: 200,
+    resizeMode: "cover",
+    marginBottom: 20,
   },
 });
 
