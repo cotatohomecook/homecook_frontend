e4708dbc7f17d39b8bb4ce203057b46232e41053
yujeongFE <yujeong6231@naver.com>
2023-07-13 01:31:39 +0900

    🎨 결제수단 선택 후 주문되도록 변경 및 handleGoBack 기능 수정

--- common/ConfirmationButton.js
+++ common/ConfirmationButton.js
@@ -1,10 +1,10 @@
 import React from "react";
 import { TouchableOpacity, Text, StyleSheet, View } from "react-native";
 
-const ConfirmationButtons = ({ onConfirm, onCancel }) => {
+const ConfirmationButtons = ({ onConfirm, onCancel, width, height }) => {
   return (
     <View style={styles.container}>
-      <View style={styles.buttonContainer}>
+      <View style={[styles.buttonContainer, { width, height }]}>
         <TouchableOpacity style={styles.yesModalbutton} onPress={onConfirm}>
           <Text style={styles.modalButtonText}>예</Text>
         </TouchableOpacity>
--- pages/PaymentScreen.js
+++ pages/PaymentScreen.js
@@ -1,29 +1,37 @@
 import React, { useState } from "react";
-import {
-  View,
-  Text,
-  StyleSheet,
-  TouchableOpacity,
-  Image,
-  Modal,
-} from "react-native";
+import { View, Text, StyleSheet, TouchableOpacity, Image } from "react-native";
 import Header from "../common/Header";
 import BackButton from "../common/BackButton";
 import { useNavigation } from "@react-navigation/native";
 import { useRoute } from "@react-navigation/native";
 import OrderButton from "../common/OrderButton";
 import CreditModalComponent from "../common/CreditModalComponent";
+import ConfirmModal from "../common/ConfirmModal";
+import { useDispatch } from "react-redux";
+import { postData } from "../store/redux/shopInfo";
+
 const PaymentScreen = () => {
   const navigation = useNavigation();
   const route = useRoute();
-  const { selectedText, totalPrice } = route.params;
+  const { selectedText, totalPrice, shopId, orderMenus, deliveryAddress } =
+    route.params;
   const [cardModalVisible, setCardModalVisible] = useState(false);
+  const [confirmModalVisible, setConfirmModalVisible] = useState(false);
+  const dispatch = useDispatch();
 
   const handleGoBack = () => {
-    navigation.navigate("OrderMenuScreen");
+    setConfirmModalVisible(true);
   };
 
-  const handleOrderPress = () => {};
+  const handleOrderPress = () => {
+    const data = {
+      shopId: shopId,
+      orderMenus: orderMenus,
+      deliveryAddress: deliveryAddress,
+    };
+
+    dispatch(postData(data));
+  };
 
   const modalVisible = () => {
     setCardModalVisible(true);
@@ -33,68 +41,89 @@ const PaymentScreen = () => {
     setCardModalVisible(false);
   };
 
+  const onConfirm = () => {
+    navigation.navigate("ShopScreen", { shopId: shopId });
+  };
+
+  const onCancel = () => {
+    setConfirmModalVisible(false);
+  };
   return (
     <>
       <Header title={"주문하기"} height={115} />
       <BackButton top={-45} onPress={handleGoBack} />
-      <View style={styles.container}>
-        <Text style={styles.title}>결제수단</Text>
-        <View style={styles.checkBox}>
-          <Text style={styles.detail}>이전에 사용했던 결제수단</Text>
-        </View>
-        <View style={styles.account} />
-        <Text style={styles.payment}>다른 결제수단 이용</Text>
-        <View style={styles.credit}>
-          <Text style={styles.creditText}>신용 / 체크카드</Text>
-        </View>
-
-        <TouchableOpacity style={styles.card} onPress={modalVisible}>
-          <View style={styles.cardContainer}>
-            <Image
-              style={styles.cardImage}
-              source={{
-                uri: "https://postfiles.pstatic.net/MjAyMzA3MDVfODkg/MDAxNjg4NTQ4MTM1MDQ4.94E78-s-FXJtrJofw8JFywsI5tyaTfGYgbpppGvUk0Qg.IdYhAsW9Ollb2yXC0htNU2gHVWiCxNInXW4frBMIetwg.PNG.jeongsi2468/image.png?type=w773",
-                width: 24,
-                height: 18,
-              }}
-            />
-            {selectedText ? (
-              <Text style={styles.cardText}>{selectedText}</Text>
-            ) : (
-              <Text style={styles.cardText}>
-                결제 가능한 카드를 선택해주세요.{" "}
-              </Text>
-            )}
+      <View style={styles.sortContainer}>
+        <View style={styles.container}>
+          <Text style={styles.title}>결제수단</Text>
+          <View style={styles.checkBox}>
+            <Text style={styles.detail}>이전에 사용했던 결제수단</Text>
           </View>
-        </TouchableOpacity>
-        <View style={styles.totalPriceContainer}>
-          <Text style={styles.totalPriceText}>총 금액</Text>
-          <Text style={styles.totalPrice}>{totalPrice}원</Text>
-          <View style={styles.seperation} />
+          <View style={styles.account} />
+          <Text style={styles.payment}>다른 결제수단 이용</Text>
+          <View style={styles.credit}>
+            <Text style={styles.creditText}>신용 / 체크카드</Text>
+          </View>
+
+          <TouchableOpacity style={styles.card} onPress={modalVisible}>
+            <View style={styles.cardContainer}>
+              <Image
+                style={styles.cardImage}
+                source={{
+                  uri: "https://postfiles.pstatic.net/MjAyMzA3MDVfODkg/MDAxNjg4NTQ4MTM1MDQ4.94E78-s-FXJtrJofw8JFywsI5tyaTfGYgbpppGvUk0Qg.IdYhAsW9Ollb2yXC0htNU2gHVWiCxNInXW4frBMIetwg.PNG.jeongsi2468/image.png?type=w773",
+                  width: 24,
+                  height: 18,
+                }}
+              />
+              {selectedText ? (
+                <Text style={styles.cardText}>{selectedText}</Text>
+              ) : (
+                <Text style={styles.cardText}>
+                  결제 가능한 카드를 선택해주세요.{" "}
+                </Text>
+              )}
+            </View>
+          </TouchableOpacity>
+          <View style={styles.totalPriceContainer}>
+            <Text style={styles.totalPriceText}>총 금액</Text>
+            <Text style={styles.totalPrice}>{totalPrice}원</Text>
+            <View style={styles.seperation} />
+          </View>
+          <View stlye={styles.orderButton}>
+            <OrderButton
+              title={"주문 완료하기"}
+              color={"#FFB15F"}
+              onPress={handleOrderPress}
+            />
+          </View>
         </View>
-        <OrderButton
-          title={"주문 완료하기"}
-          color={"#FFB15F"}
-          onPress={handleOrderPress}
-        />
       </View>
       <CreditModalComponent
         modalVisible={cardModalVisible}
         closeModal={closeModal}
         totalPrice={totalPrice}
       />
+      <ConfirmModal
+        text={"다음에 주문하시겠습니까?"}
+        onConfirm={onConfirm}
+        onCancel={onCancel}
+        width={220}
+        height={32}
+        modalVisible={confirmModalVisible}
+      />
     </>
   );
 };
 
 const styles = StyleSheet.create({
+  sortContainer: {
+    alignItems: "center",
+  },
   container: {
     width: 369,
     height: 381,
     borderRadius: 20,
     backgroundColor: "#FFFFFF",
     marginTop: 15,
-    marginLeft: 13,
   },
   title: {
     fontSize: 20,
@@ -156,7 +185,6 @@ const styles = StyleSheet.create({
     borderRadius: 20,
     backgroundColor: "#FFFFFF",
     marginTop: 70,
-    marginLeft: 3,
   },
   totalPriceText: {
     fontSize: 17,
@@ -183,6 +211,11 @@ const styles = StyleSheet.create({
     marginTop: 14,
     flexDirection: "row",
     alignItems: "center",
+  },
+  orderButton: {
+    justifyContent: "center",
+    alignItems: "center",
+    marginLeft: -10,
   },
 });
 
