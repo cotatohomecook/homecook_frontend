a52867c6da0ecdafd678c1c21569833d6a6adb6b
yujeongFE <yujeong6231@naver.com>
2023-07-27 21:32:03 +0900

    🐛 순위 출력 시 NaN으로 표시되는 문제 개선

--- components/Home/CustomerStoreRank.js
+++ components/Home/CustomerStoreRank.js
@@ -6,13 +6,13 @@ function CustomerStoreRank() {
   const [news, setNews] = useState([]);
   const [loading, setLoading] = useState(false);
   const [error, setError] = useState(null);
-  const [currentIndex, setCurrentIndex] = useState(0); //초깃값을 0으로 설정함
+  const [currentIndex, setCurrentIndex] = useState(0);
+  const [currentUser, setCurrentUser] = useState(null);
 
   const fetchNews = async () => {
     try {
       setError(null);
       setLoading(true);
-      setNews([]);
 
       const response = await axios.get(
         "http://3.38.33.21:8080/api/shops/rank?latitude=37.602643&longitude=126.924805"
@@ -27,18 +27,26 @@ function CustomerStoreRank() {
   };
 
   useEffect(() => {
+    if (news.length > 0) {
+      const validIndex = currentIndex % news.length;
+      setCurrentUser({
+        ...news[validIndex],
+        ranking: validIndex + 1,
+      });
+    }
+  }, [currentIndex, news]);
+
+  useEffect(() => {
     fetchNews();
-
     const interval = setInterval(() => {
-      setCurrentIndex((prevIndex) => (prevIndex + 1) % news.length);
+      setCurrentIndex((prevIndex) => prevIndex + 1);
     }, 2000);
-
-    return () => clearInterval(interval); // setIntervar을 사용하여 반복 작업을 정지하는 역할
+    return () => clearInterval(interval);
   }, []);
 
   if (loading)
     return (
-      <View style={{ margin: 50, width: 300, height: 200 }}>
+      <View style={{ maginTop: -50, width: 300, height: 200 }}>
         <Text>로딩 중..</Text>
       </View>
     );
@@ -50,19 +58,15 @@ function CustomerStoreRank() {
     );
   if (!news.length) return null;
 
-  const currentUser = {
-    ...news[currentIndex % news.length],
-    ranking: currentIndex + 1,
-  }; // 현재 보여줄 사용자 정보를 선택하는 코드
-
   return (
     <View style={styles.newsContainer}>
-      <View style={styles.companyHeader}>
-        <Text>
-          {currentUser && currentUser.ranking}위&nbsp;{" "}
-          {currentUser && currentUser.shopName}
-        </Text>
-      </View>
+      {currentUser && (
+        <View style={styles.companyHeader}>
+          <Text>
+            {currentUser.ranking}위&nbsp; {currentUser.shopName}
+          </Text>
+        </View>
+      )}
     </View>
   );
 }
